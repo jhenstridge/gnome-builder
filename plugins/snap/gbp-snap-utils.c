a809b209d98aad5de40001d400ea89354dc288d1
James Henstridge <james@jamesh.id.au>
2017-06-01 17:33:06 +0800

    snap: Parse snapcraft.yaml to determine the snap package name.
    Also have GbpSnapBuildSystem track changes to snapcraft.yaml in order to
    stay up to date.

--- plugins/snap/gbp-snap-utils.c
+++ plugins/snap/gbp-snap-utils.c
@@ -16,7 +16,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define G_LOG_DOMAIN "gbp-snap-utils"
+
 #include "gbp-snap-utils.h"
+
+#include <ide.h>
+#include <yaml.h>
+
+G_DEFINE_AUTO_CLEANUP_CLEAR_FUNC(yaml_parser_t, yaml_parser_delete);
+G_DEFINE_AUTO_CLEANUP_CLEAR_FUNC(yaml_document_t, yaml_document_delete);
 
 GFile *
 gbp_snap_find_snapcraft_yaml (GFile        *directory,
@@ -45,10 +53,10 @@ gbp_snap_find_snapcraft_yaml (GFile        *directory,
 }
 
 /* Find the value under a YAML mapping node that matches the given key */
-yaml_node_t *
-gbp_snap_yaml_mapping_lookup (yaml_document_t *document,
-                              yaml_node_t     *node,
-                              const char      *key_string)
+static yaml_node_t *
+mapping_lookup (yaml_document_t *document,
+                yaml_node_t     *node,
+                const char      *key_string)
 {
   yaml_node_pair_t *p;
   g_assert (node->type == YAML_MAPPING_NODE);
@@ -68,40 +76,98 @@ gbp_snap_yaml_mapping_lookup (yaml_document_t *document,
   return NULL;
 }
 
-yaml_node_t *gbp_snap_find_main_part (yaml_document_t *document)
+const char *
+get_snap_arch (void)
 {
-  yaml_node_t *root, *parts;
-  yaml_node_pair_t *p;
+  static const char *arch = NULL;
 
-  root = yaml_document_get_root_node (document);
-  if (!root)
-    return NULL;
-  g_assert (root->type == YAML_MAPPING_NODE);
-
-  /* Find the "parts" section */
-  parts = gbp_snap_yaml_mapping_lookup (document, root, "parts");
-  if (!parts || parts->type != YAML_MAPPING_NODE)
-    return NULL;
-
-  /* Find a part that uses "." as its source. */
-  for (p = parts->data.mapping.pairs.start; p != parts->data.mapping.pairs.top; p++)
+  if (!arch)
     {
-      yaml_node_t *part = yaml_document_get_node (document, p->value);
-      yaml_node_t *source = gbp_snap_yaml_mapping_lookup (document, part, "source");
+      g_autofree char *m = ide_get_system_arch ();
 
-      if (source->type != YAML_SCALAR_NODE)
-        continue;
-      if (!strncmp((const char*)source->data.scalar.value, ".",
-                   source->data.scalar.length))
-        {
-          return part;
-        }
+      /* This is based on the code in snapcraft/_options.py */
+      if (!strcmp (m, "armv7l"))
+        arch = "armhf";
+      else if (!strcmp (m, "aarch64"))
+        arch = "arm64";
+      else if (strlen (m) == 4 && m[0] == 'i' && m[2] == '8' && m[3] == '6')
+        arch = "i386";
+      else if (!strcmp (m, "ppc64le"))
+        arch = "ppc64el";
+      else if (!strcmp (m, "ppc"))
+        arch = "powerpc";
+      else if (!strcmp (m, "x86_64"))
+        arch = "amd64";
+      else if (!strcmp (m, "s390x"))
+        arch = "s390x";
+
+      if (!arch)
+        g_critical ("Unknown architecture: %s", m);
+    }
+  return arch;
+}
+
+char *
+gbp_snap_calculate_package_name (GFile         *snapcraft_yaml,
+                                 GCancellable  *cancellable)
+{
+  g_autoptr(GError) error = NULL;
+  g_autofree char *yaml_contents = NULL;
+  gsize yaml_length = 0;
+  g_auto(yaml_parser_t) parser = { 0, };
+  g_auto(yaml_document_t) doc = { 0, };
+  yaml_node_t *root, *node;
+  g_autofree char *name = NULL, *version = NULL, *arch = NULL;
+
+  IDE_ENTRY;
+
+  g_assert (G_IS_FILE (snapcraft_yaml));
+  g_assert (!cancellable || G_IS_CANCELLABLE (cancellable));
+
+  if (!g_file_load_contents (snapcraft_yaml, cancellable,
+                             &yaml_contents, &yaml_length,
+                             NULL, &error))
+    {
+      g_warning ("Could not read snapcraft.yaml: %s", error->message);
+      IDE_RETURN (NULL);
     }
 
-  /* Otherwise, pick the first part (assuming there is one) */
-  p = parts->data.mapping.pairs.start;
-  if (p == parts->data.mapping.pairs.top)
-    return NULL;
+  if (!yaml_parser_initialize (&parser))
+    IDE_RETURN (NULL);
 
-  return yaml_document_get_node (document, p->value);
+  yaml_parser_set_input_string (&parser, (const unsigned char *)yaml_contents,
+                                (gssize)yaml_length);
+
+  if (!yaml_parser_load (&parser, &doc))
+    IDE_RETURN (NULL);
+
+  root = yaml_document_get_root_node (&doc);
+
+  node = mapping_lookup (&doc, root, "name");
+  if (!node || node->type != YAML_SCALAR_NODE)
+    IDE_RETURN (NULL);
+  name = g_strndup((const char*)node->data.scalar.value,
+                   node->data.scalar.length);
+
+  node = mapping_lookup (&doc, root, "version");
+  if (!node || node->type != YAML_SCALAR_NODE)
+    IDE_RETURN (NULL);
+  version = g_strndup((const char*)node->data.scalar.value,
+                      node->data.scalar.length);
+
+  /* A snapcraft project can force the architecture choice.  This is
+   * generally only used to produce noarch snaps. */
+  node = mapping_lookup (&doc, root, "architectures");
+  if (node && node->type == YAML_SEQUENCE_NODE &&
+    node->data.sequence.items.start != node->data.sequence.items.top)
+    {
+      yaml_node_t *first = yaml_document_get_node (&doc, *node->data.sequence.items.start);
+      if (first && first->type == YAML_SCALAR_NODE)
+        arch = g_strndup((const char*)first->data.scalar.value,
+                         first->data.scalar.length);
+    }
+  if (!arch)
+    arch = g_strdup (get_snap_arch ());
+
+  return g_strdup_printf("%s_%s_%s.snap", name, version, arch);
 }
