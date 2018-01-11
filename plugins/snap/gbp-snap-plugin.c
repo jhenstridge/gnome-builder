50eb527176c82be31ff74421a0690ce0fc3fcc05
Leo Arias <leo.arias@canonical.com>
2018-01-10 20:53:39 -0600

    Update the comment with the name of the file
    ;)

--- plugins/snap/gbp-snap-plugin.c
+++ plugins/snap/gbp-snap-plugin.c
@@ -1,6 +1,6 @@
-/* gbp-flatpak-plugin.c
+/* gbp-snap-plugin.c
  *
- * Copyright (C) 2017 Canonical Ltd.
+ * Copyright (C) 2017-2018 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
